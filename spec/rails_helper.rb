9ba5c81ee73503680d7d3ea6aba1767c05aa2563
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-09-14 14:04:25 -0300

    added helper and configuration support

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,8 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -92,4 +94,9 @@ RSpec.configure do |config|
       example.run
     end
   end
+RSpec.configuration do |config|
+  # [...]
+  config.include RequestSpecHelper, type: :request
+  # [...]
+end
 end
